5a43e33da5a8c7637744637159db28d3b577ed59
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-08-15 23:34:57 +0530

    Feat: Retrieve Project List from Database and Display in List View

---
+++ src/components/ProjectsListView.jsx
@@ -0,0 +1,409 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+export default function ProjectsListView() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await axios.get(`${API_BASE_URL}api/portfolio-projects`);
+        const orderedProjects = response.data.sort((a, b) => {
+          return a.sequenceID - b.sequenceID;
+        });
+        setProjects(orderedProjects);
+        // setProjects(response.data);
+      } catch (error) {
+        console.error('An error occurred while fetching projects:', error);
+      }
+    }
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <>
+      <section className="p-3 sm:p-5">
+        {/* <div className="max-w-screen-xl px-4 mx-auto lg:px-12"> */}
+        <div className="px-4 mx-auto lg:px-12">
+          <div className="relative overflow-hidden bg-white shadow-md sm:rounded-lg">
+            {/* Search & Filter*/}
+            {/* <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+              <div className="w-full md:w-1/2">
+                <form className="flex items-center">
+                  <label htmlFor="simple-search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative w-full">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 "
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                    <input
+                      type="text"
+                      id="simple-search"
+                      className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500"
+                      placeholder="Search"
+                      required=""
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
+                <button
+                  type="button"
+                  className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
+                >
+                  <svg
+                    className="h-3.5 w-3.5 mr-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      clipRule="evenodd"
+                      fillRule="evenodd"
+                      d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    />
+                  </svg>
+                  Add product
+                </button>
+                <div className="flex items-center w-full space-x-3 md:w-auto">
+                  <button
+                    id="actionsDropdownButton"
+                    data-dropdown-toggle="actionsDropdown"
+                    className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                    type="button"
+                  >
+                    <svg
+                      className="-ml-1 mr-1.5 w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                    >
+                      <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      />
+                    </svg>
+                    Actions
+                  </button>
+                  <div
+                    id="actionsDropdown"
+                    className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44"
+                  >
+                    <ul
+                      className="py-1 text-sm text-gray-700"
+                      aria-labelledby="actionsDropdownButton"
+                    >
+                      <li>
+                        <a
+                          href="#"
+                          className="block px-4 py-2 hover:bg-gray-100"
+                        >
+                          Mass Edit
+                        </a>
+                      </li>
+                    </ul>
+                    <div className="py-1">
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Delete all
+                      </a>
+                    </div>
+                  </div>
+                  <button
+                    id="filterDropdownButton"
+                    data-dropdown-toggle="filterDropdown"
+                    className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                    type="button"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      className="w-4 h-4 mr-2 text-gray-400"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Filter
+                    <svg
+                      className="-mr-1 ml-1.5 w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                    >
+                      <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      />
+                    </svg>
+                  </button>
+                  <div
+                    id="filterDropdown"
+                    className="z-10 hidden w-48 p-3 bg-white rounded-lg shadow"
+                  >
+                    <h6 className="mb-3 text-sm font-medium text-gray-900">
+                      Choose brand
+                    </h6>
+                    <ul
+                      className="space-y-2 text-sm"
+                      aria-labelledby="filterDropdownButton"
+                    >
+                      <li className="flex items-center">
+                        <input
+                          id="apple"
+                          type="checkbox"
+                          defaultValue=""
+                          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500"
+                        />
+                        <label
+                          htmlFor="apple"
+                          className="ml-2 text-sm font-medium text-gray-900"
+                        >
+                          Apple (56)
+                        </label>
+                      </li>
+                      <li className="flex items-center">
+                        <input
+                          id="fitbit"
+                          type="checkbox"
+                          defaultValue=""
+                          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500"
+                        />
+                        <label
+                          htmlFor="fitbit"
+                          className="ml-2 text-sm font-medium text-gray-900"
+                        >
+                          Microsoft (16)
+                        </label>
+                      </li>
+                      <li className="flex items-center">
+                        <input
+                          id="razor"
+                          type="checkbox"
+                          defaultValue=""
+                          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500"
+                        />
+                        <label
+                          htmlFor="razor"
+                          className="ml-2 text-sm font-medium text-gray-900"
+                        >
+                          Razor (49)
+                        </label>
+                      </li>
+                      <li className="flex items-center">
+                        <input
+                          id="nikon"
+                          type="checkbox"
+                          defaultValue=""
+                          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500"
+                        />
+                        <label
+                          htmlFor="nikon"
+                          className="ml-2 text-sm font-medium text-gray-900"
+                        >
+                          Nikon (12)
+                        </label>
+                      </li>
+                      <li className="flex items-center">
+                        <input
+                          id="benq"
+                          type="checkbox"
+                          defaultValue=""
+                          className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500"
+                        />
+                        <label
+                          htmlFor="benq"
+                          className="ml-2 text-sm font-medium text-gray-900"
+                        >
+                          BenQ (74)
+                        </label>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+            <div className="overflow-x-auto">
+              <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-4 py-3">
+                      Project name
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Project Type
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Description
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Tech Stack
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Demo URL
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Github URL
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {projects.map(project => (
+                    <tr key={project.sequenceID} className="border-b">
+                      <td
+                        scope="row"
+                        className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {project.title}
+                      </td>
+                      <td className="px-4 py-3">{project.projectType}</td>
+                      <td className="px-4 py-3">{project.description}</td>
+                      <td className="px-4 py-3">{project.techStack.join(', ')}</td>
+                      <td className="px-4 py-3">
+                        <a href={project.demoURL} target="_blank" rel="noopener noreferrer">
+                          View Demo
+                        </a>
+                      </td>
+                      <td className="px-4 py-3">
+                        <a href={project.sourceCodeURL} target="_blank" rel="noopener noreferrer">
+                          View Code
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {/* Pagination */}
+              {/* <nav
+                className="flex flex-col items-start justify-between p-4 space-y-3 md:flex-row md:items-center md:space-y-0"
+                aria-label="Table navigation"
+              >
+                <span className="text-sm font-normal text-gray-500 ">
+                  Showing
+                  <span className="font-semibold text-gray-900 ">
+                    1
+                  </span>
+                  of
+                  <span className="font-semibold text-gray-900 ">
+                    10
+                  </span>
+                </span>
+                <ul className="inline-flex items-stretch -space-x-px">
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
+                    >
+                      <span className="sr-only">Previous</span>
+                      <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      1
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      2
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      aria-current="page"
+                      className="z-10 flex items-center justify-center px-3 py-2 text-sm leading-tight border text-primary-600 bg-primary-50 border-primary-300 hover:bg-primary-100 hover:text-primary-700"
+                    >
+                      3
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      ...
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center justify-center px-3 py-2 text-sm leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
+                    >
+                      10
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      <span className="sr-only">Next</span>
+                      <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </nav> */}
+            </div>
+          </div>
+        </div>
+      </section >
+    </>
+  );
+}
