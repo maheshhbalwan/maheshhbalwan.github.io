9e22cae0ef6e4ab01303df9248b80b414a52e1c6
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-10-22 23:40:41 +0530

    fix: Portfolio Card UI

--- src/components/ProjectsGridView.jsx
+++ src/components/ProjectsGridView.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import { ArrowIcon, GitHubIcon, YoutubeIcon } from './Icons';
+import { GitHubIcon, YoutubeIcon } from './Icons';
 import axios from 'axios';
-
+import { LuGlobe } from "react-icons/lu";
 import YouTubePlayer from './YouTubePlayer';
 
 const API_BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -14,7 +14,7 @@ export default function ProjectsGridView() {
   useEffect(() => {
     async function fetchProjects() {
       try {
-        const response = await axios.get(`${API_BASE_URL}api/portfolio-projects`); // Update the endpoint URL accordingly
+        const response = await axios.get(`${API_BASE_URL}api/portfolio-projects`);
         const orderedProjects = response.data.sort((a, b) => {
           return a.sequenceID - b.sequenceID;
         });
@@ -38,56 +38,70 @@ export default function ProjectsGridView() {
   };
   return (
     <>
-      <div className="min-h-screen p-8">
-        <div className="px-4 py-8 mx-auto sm:py-16 lg:px-6">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-            {projects.map((project) => (
-              <div key={project._id} className="p-4 bg-white rounded shadow-md">
-                <img src={project.imageURL} alt={project.title} className="w-full mb-4 rounded" />
-                <h2 className="mb-2 text-xl font-semibold">{project.title}</h2>
-                <p className="mb-2 text-gray-600">{project.description}</p>
-                <div className="flex justify-between">
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {projects.filter((project) => project.visibility === true)
+            .map((project) => (
+              <div key={project._id} className="flex flex-col h-full bg-white border border-gray-200 shadow-sm group rounded-xl">
+                <div className="flex flex-col items-center justify-center h-52 rounded-t-xl">
+                  <img className='rounded-t-xl' src={project.imageURL} alt="" />
+                </div>
+                <div className="p-4 md:p-6">
+                  <h3 className="mt-4 text-xl font-semibold text-gray-800">
+                    {project.title}
+                  </h3>
+                  <p className="text-gray-500">
+                    {project.description}
+                  </p>
+                  <div className='flex flex-wrap gap-2 mt-2'>
+                    {project.techStack.map((tech) => (
+                      <span key={tech} className="inline-flex justify-center items-center gap-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
+                  <div className='mt-2'>
+                    <span className="block mb-1 text-xs font-semibold text-blue-600 uppercase">
+                      {project.projectType}
+                    </span>
+                  </div>
+                  {/* <div className='mt-2'>
+                    <span className="block mb-1 text-xs font-semibold text-blue-600 uppercase">
+                      {project.visibility ? 'Visible' : 'Hidden'}
+                    </span>
+                  </div> */}
+                </div>
+                <div className="flex mt-auto border-t border-gray-200 divide-x divide-gray-200">
                   {project.sourceCodeURL && (
-                    <a href={project.sourceCodeURL} className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-primary-100 lg:h-12 lg:w-12" target="_blank" rel="noreferrer">
+                    <a
+                      className="inline-flex items-center justify-center w-full gap-2 px-4 py-3 text-sm font-medium text-gray-700 align-middle transition-all bg-white shadow-sm rounded-bl-xl hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 sm:p-4"
+                      href={project.sourceCodeURL}
+                    >
                       <GitHubIcon />
+                      Code
                     </a>
                   )}
                   {project.demoURL && (
-                    <a href={project.demoURL} className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-primary-100 lg:h-12 lg:w-12" target="_blank" rel="noreferrer">
-                      <ArrowIcon />
+                    <a
+                      className="inline-flex items-center justify-center w-full gap-2 px-4 py-3 text-sm font-medium text-gray-700 align-middle transition-all bg-white shadow-sm rounded-br-xl hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 sm:p-4"
+                      href={project.demoURL}
+                    >
+                      <LuGlobe />
+                      Deployment
                     </a>
                   )}
                   {project.youtubeURL && (
                     <a
                       onClick={() => openVideoModal(project.youtubeURL)}
-                      className="flex items-center justify-center w-10 h-10 mb-4 rounded-full cursor-pointer bg-primary-100 lg:h-12 lg:w-12"
-                      target="_blank"
-                      rel="noreferrer"
+                      className="inline-flex items-center justify-center w-full gap-2 px-4 py-3 text-sm font-medium text-gray-700 align-middle transition-all bg-white shadow-sm rounded-br-xl hover-bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 sm:p-4"
                     >
                       <YoutubeIcon />
+                      Youtube
                     </a>
                   )}
                 </div>
-                <div className="flex flex-wrap mt-2">
-                  {project.techStack.map((tech) => (
-                    <span key={tech} className="px-2 py-1 mb-2 mr-2 text-sm text-gray-600 bg-gray-200 rounded-full">
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-                <div className="mt-2">
-                  <span className={`px-2 py-1 text-sm rounded ${project.visibility ? 'bg-green-300' : 'bg-red-300'}`}>
-                    {project.visibility ? 'Visible' : 'Hidden'}
-                  </span>
-                </div>
-                <div className="mt-2">
-                  <span className="px-2 py-1 text-sm text-gray-600 bg-gray-200 rounded-full">
-                    {project.projectType}
-                  </span>
-                </div>
               </div>
             ))}
-          </div>
         </div>
         <YouTubePlayer isOpen={modalOpen} videoId={currentVideoId} closeModal={closeVideoModal} />
       </div>
